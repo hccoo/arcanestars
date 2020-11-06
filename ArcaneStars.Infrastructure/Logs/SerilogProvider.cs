afec2923b24a1b082eb587813fcdd329d79fab64
hccoo@outlook.com <hccoo@outlook.com>
2020-11-07 07:45:10 +0800

    add logic

---
+++ ArcaneStars.Infrastructure/Logs/SerilogProvider.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcaneStars.Infrastructure.Logs
+{
+    public class SerilogProvider
+    {
+        public static void StartWithMysql(string connectionString,string tableName="app_logs")
+        {
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Information()
+                        .WriteTo.MySQL(connectionString,tableName)
+                        .CreateLogger();
+        }
+    }
+}
